c571fd4e8f2933e7d8181091d47dac42f25ccc39
psarangi550 <psarangi50@gmail.com>
2021-10-13 05:03:45 +0530

    Commit on 13/10/2021

--- Geeky_Show_Tut/DjangoProjects/ManytoOneRelationProject/ManytoOneRelationApp/admin.py
+++ Geeky_Show_Tut/DjangoProjects/ManytoOneRelationProject/ManytoOneRelationApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post #importing models from models.py file
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):#defining the Model Admin class for the same
+    list_display=["id","post_cat","post_title","post_created_date","user"]
+    #defining all the required field to diplay on the model field
--- Geeky_Show_Tut/DjangoProjects/ManytoOneRelationProject/ManytoOneRelationApp/models.py
+++ Geeky_Show_Tut/DjangoProjects/ManytoOneRelationProject/ManytoOneRelationApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User# importing the User Model
 # Create your models here.
+class Post(models.Model):#creating the Model Class Post
+    user=models.ForeignKey(User,on_delete=models.SET_DEFAULT,default="1")
+    #creating the Foreign to display the fields
+    post_cat=models.CharField(max_length=50)
+    #defining the CharField For the Same
+    post_title=models.CharField(max_length=50)
+    #defining the fields for the post model
+    post_created_date=models.DateField()
+    #defining the Datefield for the model class
+
+    def __str__(self):
+        return self.post_title
+        #defining the reporesentation of the Model Object
