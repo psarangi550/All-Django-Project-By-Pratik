a576318651a7333a00173cc2d1a9a2c0f2cd0936
psarangi550 <psarangi50@gmail.com>
2021-10-14 05:32:17 +0530

    Again Modified

--- classBasedViews/TemplateViewProject/TemplateViewApp4/views.py
+++ classBasedViews/TemplateViewProject/TemplateViewApp4/views.py
@@ -6,7 +6,7 @@ class MyTemplateDynamicontextView(TemplateView):
     template_name="TemplateViewApp4/dynamic.html"
     def get_context_data(self, **kwargs):
         id=kwargs.get("id")
-        # context =
+        # context =super().get_context_data(**kwargs)
         # context["name"]="Rika"
         # context["salary"]=50000
         # context["id"]=id
